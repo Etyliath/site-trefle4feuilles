c0781613bb6503d0f738a72f4ed7511cee592fbe
Edouard <esdasilva@me.com>
2024-09-20 15:53:56 +0200

    add function user and authentication Issue #5

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/admin/dashboard', name: 'admin.dashboard.')]
 class DashboardController extends AbstractController
 {
     #[Route('/', name: 'index', methods: ['GET'])]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager, UserPasswordHasherInterface $hasher): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         return $this->render('admin/dashboard/index.html.twig');
     }
 
