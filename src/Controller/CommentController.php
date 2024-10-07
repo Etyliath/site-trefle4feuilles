6edbc4fed4bf192d00afcff3d0b29220566aabb3
Edouard <esdasilva@me.com>
2024-10-07 21:48:53 +0200

    update view to prepare add feature order Issue #19

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -11,10 +11,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/comment', name: 'comment.')]
 class CommentController extends AbstractController
 {
+    #[isGranted('ROLE_USER')]
     #[Route('/{id}', name: 'new', methods: ['POST','GET'])]
     public function new (
         Request $request,
